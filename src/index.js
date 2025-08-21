0539f92e5dcfd6896350525698a626a82ba20345
anilbameta <anilbameta@gmail.com>
2025-08-21 13:31:54 +0530

    util code changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import dotenv from 'dotenv';
 import dbConnect from './db/index.js';
+import app from './app.js';
 
 dotenv.config()
 
-dbConnect();
+dbConnect()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server listening on port ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("Error in DB", err)
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+// const asyncHandler = (fn) => async(req,res,err) => {
+//   try {
+//     await fn(req,res,next)
+//   }
+//   catch(err) {
+//     res.status(err.code || 500).json({
+//         success: false,
+//         message: err.message
+//     })
+//   }
+// }
+
+const asyncHandler = (requestHandler) => {
+(req,res,next) => {
+    Promise.resolve(requestHandler(req,res,next))
+    .catch(err => next(err))
+}
+}
+
+export default asyncHandler;
