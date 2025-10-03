d77d9a11d09b80ef9fffd7030f846256f27c7a44
anilbameta <anilbameta@gmail.com>
2025-10-03 22:52:43 +0530

    update video details and update views controllers and routes

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -63,15 +63,49 @@ const getVideoList = asyncHandler(async (req, res) => {
 
 const deleteVideo = asyncHandler(async (req, res) => {
   const { id } = req.query;
-  if(!id) throw new ApiError(404, 'video Id is required');
+  if (!id) throw new ApiError(404, "Video Id is required");
   const video = await Video.findById(id);
-  if(!video) throw new ApiError(400, "Please enter valid video Id");
-  if(!video.owner.equals(req.user?._id))
-     throw new ApiError(403,"You are not allowed to delete the video");
-    await video.deleteOne();
-    return res
+  if (!video) throw new ApiError(400, "Please enter valid video Id");
+  if (!video.owner.equals(req.user?._id))
+    throw new ApiError(403, "You are not allowed to delete the video");
+  await video.deleteOne();
+  return res
     .status(200)
     .json(new ApiResponse(200, {}, "Video deleted successfully"));
 });
 
-export { addVideo, getVideoList, deleteVideo };
+const updateVideoDetails = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body || {};
+  if (!title && !description)
+    throw new ApiError(400, "Enter title or description details");
+  if (!id) throw new ApiError(404, "Video Id is required");
+  const videoDetails = await Video.findById(id);
+  if (!videoDetails) throw new ApiError(400, "Please enter valid video Id");
+  if (!videoDetails.owner.equals(req.user?._id))
+    throw new ApiError(403, "You are not allowed to delete the video");
+  if (title) videoDetails.title = title;
+  if (description) videoDetails.description = description;
+  await videoDetails.save();
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, videoDetails, "Video details updated successfully")
+    );
+});
+
+const updateViews = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  if (!id) throw new ApiError(404, "Video Id is required");
+  const videoDetails = await Video.findById(id);
+  if (!videoDetails) throw new ApiError(400, "Please enter valid video Id");
+  videoDetails.views = videoDetails.views + 1;
+  await videoDetails.save();
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, videoDetails, "Video views updated successfully")
+    );
+});
+
+export { addVideo, getVideoList, deleteVideo, updateVideoDetails, updateViews };
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { addVideo, deleteVideo, getVideoList } from "../controllers/video.controller.js";
+import { addVideo, deleteVideo, getVideoList, updateVideoDetails, updateViews } from "../controllers/video.controller.js";
 
 const router = Router();
 
+router.route("/update-video-views/:id").put(updateViews);
+
+//protected routes
 router.route("/add-video").post(verifyJwt,  upload.fields([
     {
       name: "videoFile",
@@ -19,6 +22,10 @@ router.route("/add-video").post(verifyJwt,  upload.fields([
 router.route("/video-list").get(verifyJwt,getVideoList);
 
 router.route("/delete-video").delete(verifyJwt,deleteVideo);
+
+router.route("/update-video-details/:id").patch(verifyJwt,updateVideoDetails);
+
+
 
 
   export default router;
